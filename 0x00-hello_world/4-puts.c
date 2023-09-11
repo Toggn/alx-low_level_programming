983b2626728a0aa17c3b624b6b17e739e983b4aa
yyousssef <youse324@gmail.com>
2023-09-11 04:56:08 -0700

    as

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,16 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Description A C program that points with put function
+ * Description: A C program that prints a line with the puts function
  *
- * Return : Alawys 0 (Success)
-*/ 
-
-
+ * Return: 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
 
