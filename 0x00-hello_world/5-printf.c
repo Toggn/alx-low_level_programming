8aa55bbf8548de2e92c6283f54c446765dae67e5
yyousssef <youse324@gmail.com>
2023-09-11 05:44:26 -0700

    as

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,18 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main() {
-    printf("This is a piece of art,\n");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a line with the printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("This is a piece of art,\n");
+return (0);
 }
 
-gcc -Wall 5-printf.c -o 5-printf.c
+gcc '-Wall' '5-printf.c' '-o' '5-printf'
+
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,22 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main() {
-    printf("Size of char: %zu bytes\n", sizeof(char));
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of long: %zu bytes\n", sizeof(long));
-    printf("Size of long long: %zu bytes\n", sizeof(long long));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of long double: %zu bytes\n", sizeof(long double));
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a line with the sizeof  function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
 
-    return 0;
+return ('0');
 }
 
