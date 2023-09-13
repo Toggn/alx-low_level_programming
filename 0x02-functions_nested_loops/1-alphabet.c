df54674135eb50164ab4d5d3f08f35a6e0d4abaf
yyousssef <youse324@gmail.com>
2023-09-13 05:08:02 -0700

    kk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,32 +4,16 @@
  * print_alphabet - Function to print the alphabet from 'a' to 'z' with a newline at the end
  *
  * Return: Nothing (void)
- */
-#include "main.h"
-
-/**
- * main - check the code
  *
- * Return: Always 0.
- */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
-
+*/
 
 void print_alphabet(void)
-
 {
-char letter = 'a';
-
-while (letter <= 'z')
+for (char letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
-letter++;
 }
-_putchar('\n'); 
+_putchar('\n');
 }
+
 
