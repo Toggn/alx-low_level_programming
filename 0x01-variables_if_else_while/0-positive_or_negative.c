0874dd0dcf0b040232213327469bbd1c906752a5
yyousssef <youse324@gmail.com>
2023-10-02 07:31:34 -0700

    kk

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
 {
-printf("%d this mnuber is positive " , n);
+printf("%d is positive " , n);
 
 }
 else if(n==0)
 {
-printf("%d this mnuber is zero " , n);
+printf("%d  is zero " , n);
 }
 else
 {
-printf("%d this mnuber is negative " , n);
+printf("%d  is negative " , n);
 }
 
 return (0);
