ef8eb90e6e6cabf574695b0692506d8458ba4964
yyousssef <youse324@gmail.com>
2023-10-10 11:26:03 -0700

    dmd

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printf the name of the file , followed by a new line
+ *
+ * Return : 0(success)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
