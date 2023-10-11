eb3ba5bd52a17d7859cea14fd39ca0465540fdcc
yyousssef <youse324@gmail.com>
2023-10-11 11:10:24 -0700

    ww

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,12 @@
 #define DOG_H
 
 
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-typedef struct dog dog_t
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d)i;
 
 
 #endif
