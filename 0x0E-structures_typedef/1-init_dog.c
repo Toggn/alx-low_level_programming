c75e1a29ecc201f9e37d3649850134dfbaffe473
youssef <youwe2097@gmail.com>
2023-10-23 05:29:51 -0700

    ww

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,18 @@
 #include "dog.h"
 #include <stdio.h>
 
-
+/**
+* init_dog - inits a variable of
+*type dog
+*@d: dog identification
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  d->name = name;
-  d->age = age;
-  d->owner = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
 
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,20 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+* print_dog - prints a struct dog
+*@d: dog identification
+*
+*/
+
+
 void print_dog(struct dog *d)
 {
-if (d==NULL)
+if (d == NULL)
 {
 exit(1);
-}else
+}
+else
 {
 printf("Dog name: %s\n", d->name);
 printf("Dog age: %f\n", d->age);
