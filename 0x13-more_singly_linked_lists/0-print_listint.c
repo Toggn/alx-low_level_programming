f2ce2712b1c95a328b5a7a0f6116e13665a30fa3
youssef <youwe2097@gmail.com>
2023-10-24 05:08:01 -0700

    ww

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
- * @h: head of a list.
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * Return: numbers of nodes.
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t you = 0;
+size_t num = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		you++;
-	}
-	return (you);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
+
+return (num);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,24 +1,19 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in
- * a linked list.
- * @h: head of a list.
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * Return: numbers of you.
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
-
 {
-	size_t you = 0;
+size_t num = 0;
 
-	while (h != NULL)
-	{
-		h = h->next;
-		you++;
-	}
-	return (you);
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
 }
